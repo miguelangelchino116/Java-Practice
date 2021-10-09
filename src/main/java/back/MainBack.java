cf47282df3fba1269b51e955f7b8443ed0645c14
Miguel Angel Cespedes Hernandez <miguelangelchino116@gmail.com>
2021-10-08 22:26:12 -0300

    First commit

--- src/main/java/back/MainBack.java
+++ src/main/java/back/MainBack.java
@@ -1,9 +1,32 @@
 package back;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 
 public class MainBack {
     public static void main(String[] args) throws IOException {
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+
+        ArrayList<rootRow> rowArrayList = mapper.readValue(new URL("https://lkqd.com/sellers.json"), new ObjectMapper().getTypeFactory().constructCollectionType(ArrayList.class, rootRow.class));
+
+        for (rootRow row : rowArrayList) {
+            System.out.println(row.getVersion());
+            System.out.println(row.getContact_email());
+            System.out.println(row.getContact_address());
+            System.out.println(row.getIdentifiers());
+            System.out.println(row.getSellers());
+            System.out.println("--------------------");
+        }
+//        OutputResponse outputfile = new OutputResponse();
+//
+//        outputfile.writeResponse(rowArrayList);
     }
 }
---
+++ src/main/java/back/rootRow.java
@@ -0,0 +1,50 @@
+package back;
+
+public class rootRow {
+    private String version;
+    private String contact_email;
+    private String contact_address;
+    private String identifiers;
+    private String sellers;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getContact_email() {
+        return contact_email;
+    }
+
+    public void setContact_email(String contact_email) {
+        this.contact_email = contact_email;
+    }
+
+    public String getContact_address() {
+        return contact_address;
+    }
+
+    public void setContact_address(String contact_address) {
+        this.contact_address = contact_address;
+    }
+
+    public String getIdentifiers() {
+        return identifiers;
+    }
+
+    public void setIdentifiers(String identifiers) {
+        this.identifiers = identifiers;
+    }
+
+    public String getSellers() {
+        return sellers;
+    }
+
+    public void setSellers(String sellers) {
+        this.sellers = sellers;
+    }
+}
+
